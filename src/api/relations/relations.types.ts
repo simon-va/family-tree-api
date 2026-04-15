ebcec2aab57f7bbda87f5110d6f9bbd4057c7894
Simon Vahlbrock <simon.vahlbrock@icloud.com>
2026-04-15 14:01:56 +0200

    Updates fuzzyDate Resource

--- src/api/relations/relations.types.ts
+++ src/api/relations/relations.types.ts
@@ -1,10 +1,12 @@
-import type { FuzzyDateResource } from '../../resources/fuzzy-dates/fuzzyDate.model.js';
+import type { FuzzyDateFieldPrecision, FuzzyDateResource } from '../../resources/fuzzy-dates/fuzzyDate.model.js';
 import type { RelationshipResource } from '../../resources/relations/relation.model.js';
 
 export interface CreateFuzzyDateInput {
   precision: FuzzyDateResource['precision'];
   date: string;
+  datePrecision?: FuzzyDateFieldPrecision;
   dateTo?: string;
+  dateToPrecision?: FuzzyDateFieldPrecision;
   note?: string;
 }
 
